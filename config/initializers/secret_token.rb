fe4d95d9fc4aa1983e32756fe4710269f7cf68ed
Nima Gardideh <nima@halfmoon.ws>
2012-10-16 09:51:24 -0400

    added rails repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileTokenGenerator::Application.config.secret_token = 'ce13d31b63f76c5c9f53d7b2162b7b8b03a077c2c4f07525cf794813696a6dacf31a387a2e39ed31a59e0acadefb77cfb89f0c644a28ee113010c1f58d6c4a2b'
